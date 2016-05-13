b2fd989316fde58fbbdd1a9d1a5dfa0a878a410b
GonLou <goncalo.lourenco@outlook.com>
2016-05-13 10:21:20 +0100

    Particle class with variables done.

---
+++ Particles/Assets/Scripts/Particle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle
+{
+
+    struct pVector
+    {
+        Vector3 location;
+        float velocity;
+        float acceleration;
+    }
+
+    float lifeSpan;
+
+    bool isDead()
+    {
+        if (lifeSpan < 0.0f)
+            return true;
+        else
+            return false;
+    }
+}
