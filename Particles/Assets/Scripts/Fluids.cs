797ca83b6579992aaf8275bb5d84a956c35eed58
BaggioPereira <baggio_111111@hotmail.com>
2016-05-29 19:35:10 +0100

    Variables set

---
+++ Particles/Assets/Scripts/Fluids.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fluids : MonoBehaviour {
+    public Material fluidMat;
+
+    RenderTexture GUITexture, divergenceTexture, obstaclesTexture;
+    RenderTexture[] velocityTexture, densityTexture, pressureTexture, temperatureTexture;
+
+    const float cellSize = 1.25f;
+
+    const float ambientTemperature = 0.0f;
+    const float impulseTemperature = 10.0f;
+    const float impulseDensity = 1.0f;
+    const int JacobiIterations = 40;
+    const float timeStep = 0.125f;
+    const float smokeBuoyancy = 1.0f;
+    const float smokeWeight = 0.05f;
+    const float gradientScale = 1.125f / cellSize;
+    const float temperatureDissipation = 0.99f;
+    const float velocityDissipation = 0.99f;
+    const float densityDissipation = 0.9999f;
+
+    Vector2 impulsePosition = new Vector2(0.5f, 0.5f);
+    float inpulseSize = 0.1f;    
+    
+    float viewWidth, viewHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
