2b6c54732572edec2d830b0c813516704cdb255f
BaggioPereira <baggio_111111@hotmail.com>
2016-05-30 00:29:49 +0100

    RenderTexture creation

--- Particles/Assets/Scripts/Fluids.cs
+++ Particles/Assets/Scripts/Fluids.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Fluids : MonoBehaviour {
-    public Material fluidMat;
+    public Material fluidMat, GUImat;
 
     RenderTexture GUITexture, divergenceTexture, obstaclesTexture;
     RenderTexture[] velocityTexture, densityTexture, pressureTexture, temperatureTexture;
@@ -30,16 +30,42 @@ public class Fluids : MonoBehaviour {
     GUITexture GUI;
     
     float viewWidth, viewHeight;
+    Vector2 inverseSize;
 
 	// Use this for initialization
 	void Start () 
     {
         gameObject.AddComponent<GUITexture>();
         GUI = GetComponent<GUITexture>();
-        if(GUI)
-        {
-            Debug.Log("Has got a gui texture");
-        }
+        Rect GUIInset = new Rect(-256, -256, 512, 512);
+        GUI.pixelInset = GUIInset;
+
+        viewWidth = GUI.pixelInset.width;
+        viewHeight = GUI.pixelInset.height;
+        inverseSize = new Vector2(1.0f / viewWidth, 1.0f / viewHeight);
+
+        GUITexture = new RenderTexture((int)viewWidth, (int)viewHeight, 0, RenderTextureFormat.ARGB32);
+        GUITexture.filterMode = FilterMode.Bilinear;
+        GUITexture.wrapMode = TextureWrapMode.Clamp;
+        GUITexture.Create();
+
+        divergenceTexture = new RenderTexture((int)viewWidth, (int)viewHeight, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
+        divergenceTexture.filterMode = FilterMode.Point;
+        divergenceTexture.wrapMode = TextureWrapMode.Clamp;
+        divergenceTexture.Create();
+
+        obstaclesTexture = new RenderTexture((int)viewWidth, (int)viewHeight, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
+        obstaclesTexture.filterMode = FilterMode.Point;
+        obstaclesTexture.wrapMode = TextureWrapMode.Clamp;
+        obstaclesTexture.Create();
+
+        velocityTexture = new RenderTexture[2];
+        densityTexture = new RenderTexture[2];
+        temperatureTexture = new RenderTexture[2];
+        pressureTexture = new RenderTexture[2];
+
+        GUI.texture = GUITexture;
+        GUImat.SetTexture("Obstacles", obstaclesTexture);
 	}
 	
 	// Update is called once per frame
